61270fb44814cbb1500b2dbd3bd416384f579159
Nishchal Gond <ngond49@gmail.com>
2025-12-27 21:12:31 +0530

    Updated project changes

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -3,31 +3,29 @@ import { config } from "@/data/config";
 import { Resend } from "resend";
 import { z } from "zod";
 
+
+
 const Email = z.object({
   fullName: z.string().min(2, "Full name is invalid!"),
   email: z.string().email({ message: "Email is invalid!" }),
   message: z.string().min(10, "Message is too short!"),
 });
-
 export async function POST(req: Request) {
   try {
     const resend = new Resend(process.env.RESEND_API_KEY);
     const body = await req.json();
     console.log(body);
-
     const {
       success: zodSuccess,
       data: zodData,
       error: zodError,
     } = Email.safeParse(body);
-
-    if (!zodSuccess) {
+    if (!zodSuccess)
       return Response.json({ error: zodError?.message }, { status: 400 });
-    }
 
     const { data: resendData, error: resendError } = await resend.emails.send({
-      from: "Portfolio <onboarding@resend.dev>",
-      to: ["gond.nishchal1@gmail.com"], // 🔥 Updated only this line
+      from: "Porfolio <onboarding@resend.dev>",
+      to: [config.email],
       subject: "Contact me from portfolio",
       react: EmailTemplate({
         fullName: zodData.fullName,
